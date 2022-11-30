9f7b0bc97616a6442871cce397eaa575eaf5a2cd
tl <liangtian@2zetta.com>
2022-11-30 21:40:53 +0800

    增加vpcflow logs format参数，默认为all

--- variables.tf
+++ variables.tf
@@ -237,6 +237,10 @@ variable "vpc_flow_logs_s3_key_prefix" {
   default     = "flow-logs"
 }
 
+variable "vpc_flow_logs_log_format" {
+  description = "The log_format of VPC Flow Logs"
+  default     = "$${account-id} $${action} $${az-id} $${bytes} $${dstaddr} $${dstport} $${end} $${flow-direction} $${instance-id} $${interface-id} $${log-status} $${packets} $${pkt-dst-aws-service} $${pkt-dstaddr} $${pkt-src-aws-service} $${pkt-srcaddr} $${protocol} $${region} $${srcaddr} $${srcport} $${start} $${sublocation-id} $${sublocation-type} $${subnet-id} $${tcp-flags} $${traffic-path} $${type} $${version} $${vpc-id}"
+}
 
 # --------------------------------------------------------------------------------------------------
 # Variables for config-baseline module.
